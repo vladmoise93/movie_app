01acc40caf15c288dbd30cc1f574f8619f682476
Vlad Moise <vladmoise93>
2020-08-04 11:44:44 +0300

    Updates

---
+++ precache-manifest.63b9b754fb49e314353c38193cfb1283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26f756f4edafbb969e6ce55ab086e36",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "30bcdea7ba7e5529775e",
+    "url": "/movie_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc8de6eaa527848b2c17",
+    "url": "/movie_app/static/js/2.2a0e1b37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2a0e1b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30bcdea7ba7e5529775e",
+    "url": "/movie_app/static/js/main.625cfa49.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
